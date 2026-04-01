3ea440cca9dbf8b41c42bbe9d98c92505ced8a1c
Jarek.Mankiewicz <mmagari@wp.pl>
2026-04-01 21:08:22 +0200

    initial project setup with vite and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-100">
+      <h1 className="text-3xl font-bold text-slate-900">
+        Tailwind działa 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
