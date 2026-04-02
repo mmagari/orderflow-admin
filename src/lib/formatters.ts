b5c50e616c8285b8bfc54feca3ca2fceed9fea4e
Jarek.Mankiewicz <mmagari@wp.pl>
2026-04-02 12:39:45 +0200

    add order formatters

---
+++ src/lib/formatters.ts
@@ -0,0 +1,13 @@
+export function formatCurrency(value: number) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(value)
+}
+
+export function formatDate(dateString: string) {
+  return new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'medium',
+    timeStyle: 'short',
+  }).format(new Date(dateString))
+}
