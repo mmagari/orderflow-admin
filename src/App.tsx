cf253b2441030193136314340807e5889f0bcf09
Jarek.Mankiewicz <mmagari@wp.pl>
2026-04-04 18:16:10 +0200

    add global toaster configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import { router } from './router'
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            borderRadius: '12px',
+            border: '1px solid #e2e8f0',
+            background: '#ffffff',
+            color: '#0f172a',
+          },
+          success: {
+            duration: 2500,
+          },
+          error: {
+            duration: 4000,
+          },
+        }}
+      />
+    </>
+  )
 }
 
 export default App
